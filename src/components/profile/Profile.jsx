a132be22ebee17a00487810de759ceca87dac032
Asik-ali <asik14923@gmail.com>
2023-10-22 16:59:04 +0530

    FIRST

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,22 +1,19 @@
-
-
 import React, { useState, useEffect, useContext } from 'react';
 import myContext from '../../context/data/myContext';
 
 function Profile() {
-  const context = useContext(myContext)
-  const { mode} = context
+  const context = useContext(myContext);
+  const { mode } = context;
   const userData = JSON.parse(localStorage.getItem('user'));
   const [orderCount, setOrderCount] = useState(0);
 
   useEffect(() => {
-
+    // You can add any additional logic here if needed
   }, [userData]);
-
 
   const logout = () => {
     localStorage.clear('user');
-    window.location.href = '/login'
+    window.location.href = '/login';
   }
 
   return (
@@ -24,10 +21,10 @@ function Profile() {
       <h2 className="text-2xl font-bold mb-4">User Profile</h2>
       {userData ? (
         <div>
-          <p className="text-lg mb-2">Email: {userData.user.email}</p>
+          <p className="text-lg mb-2">Name: {userData.user.name}</p>
+          <p className="text-lg">Email: {userData.user.email}</p>
           <p className="text-lg">UID: {userData.user.uid}</p>
           <p className="text-lg">Orders: {orderCount}</p>
-
           {/* Additional user information can be displayed here */}
         </div>
       ) : (
@@ -35,7 +32,7 @@ function Profile() {
       )}
 
       <div className="flow-root bg-black w-28 btn">
-        <a onClick={logout} className="-m-2 block  font-medium cursor-pointer text-white p-4  " style={{ color: mode === 'dark' ? 'white' : '', }}>
+        <a onClick={logout} className="-m-2 block font-medium cursor-pointer text-white p-4" style={{ color: mode === 'dark' ? 'white' : '' }}>
           Logout
         </a>
       </div>
@@ -44,5 +41,3 @@ function Profile() {
 }
 
 export default Profile;
-
-
