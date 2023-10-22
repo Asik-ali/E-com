4c668cd96e02d78c2c6ec1f389f02b93eff27a18
Asik-ali <asik14923@gmail.com>
2023-10-22 16:37:06 +0530

    FIRST

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -7,8 +7,14 @@ function Profile() {
   const [orderCount, setOrderCount] = useState(0);
 
   useEffect(() => {
-    
+
   }, [userData]);
+
+
+  const logout = () => {
+    localStorage.clear('user');
+    window.location.href = '/login'
+  }
 
   return (
     <div className="max-w-xl mx-auto mt-10 p-6 bg-white rounded-lg shadow-lg">
@@ -18,12 +24,18 @@ function Profile() {
           <p className="text-lg mb-2">Email: {userData.user.email}</p>
           <p className="text-lg">UID: {userData.user.uid}</p>
           <p className="text-lg">Orders: {orderCount}</p>
-          
+
           {/* Additional user information can be displayed here */}
         </div>
       ) : (
         <p className="text-lg">No user data available</p>
       )}
+
+      <div className="flow-root">
+        <a onClick={logout} className="-m-2 block p-2 font-medium text-gray-900 cursor-pointer" style={{ color: mode === 'dark' ? 'white' : '', }}>
+          Logout
+        </a>
+      </div>
     </div>
   );
 }
